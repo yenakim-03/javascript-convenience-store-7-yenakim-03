25f25d9386a0e898f86a8740b1ebe53636684191
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-10 21:30:37 +0900

    feat: Product 클래스 생성

---
+++ src/Product.js
@@ -0,0 +1,15 @@
+class Product {
+  #name;
+  #price;
+  #quantity;
+  #promotion;
+
+  constructor(name, price, quantity, promotion) {
+    this.#name = name;
+    this.#price = price;
+    this.#quantity = quantity;
+    this.#promotion = promotion;
+  }
+}
+
+export default Product;
