df74f5ce52179fae18e1736a137b3bbe0c8f78b4
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-11 09:34:51 +0900

    feat: 환영 인사 출력 메서드 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,6 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class OutputView {
+  static printWelcomeMessage() {
+    Console.print('안녕하세요. W편의점입니다.');
+    Console.print('현재 보유하고 있는 상품입니다.\n');
+  }
+
   static printProduct(product) {
     const name = product.getName();
     const price = this.formatPrice(product.getPrice());
