b197842624b3f0a3d70f83e3d755aeddf16181fa
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-11 23:05:13 +0900

    feat: 입력 메서드

--- src/InputView.js
+++ src/InputView.js
@@ -30,7 +30,7 @@ class InputView {
   static async inputProductAndQuantity() {
     while (true) {
       try {
-        const input = await Console.readLineAsync('\n[구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n');
+        const input = await Console.readLineAsync('\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n');
         this.validateProductAndQuantity(input);
         return this.parseProductAndQuantity(input);
       } catch (error) {
@@ -64,6 +64,42 @@ class InputView {
     while (true) {
       try {
         const input = await Console.readLineAsync(`현재 ${productName}은(는) ${quantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`);
+        this.validateYesNoInput(input);
+        return input;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  static async confirmRegularPrice(productName, quantity) {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(`현재 ${productName} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`);
+        this.validateYesNoInput(input);
+        return input;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  static async confirmMembershipDiscount() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync('멤버십 할인을 받으시겠습니까? (Y/N)\n');
+        this.validateYesNoInput(input);
+        return input;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  static async askForMorePurchase() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync('감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n');
         this.validateYesNoInput(input);
         return input;
       } catch (error) {
