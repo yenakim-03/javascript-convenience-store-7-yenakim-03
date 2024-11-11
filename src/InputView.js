6c2c956a63a85e48fe15c342a41bd90907a98ce2
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-11 14:30:00 +0900

    feat: 구매 상품 및 수량 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { Console } from '@woowacourse/mission-utils';
 
 class InputView {
   static readList(path) {
@@ -23,6 +24,38 @@ class InputView {
         obj[key.trim()] = values[index].trim();
         return obj;
       }, {});
+    });
+  }
+
+  static async inputProductAndQuantity() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync('\n[구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n');
+        this.validateProductAndQuantity(input);
+        return this.parseProductAndQuantity(input);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  static validateProductAndQuantity(input) {
+    const pattern = /\[([^,]+)-(\d+)\]/;
+
+    input.split(',').forEach((item) => {
+      if (!pattern.test(item)) {
+        throw new Error('[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.');
+      }
+    });
+  }
+
+  static parseProductAndQuantity(input) {
+    const data = [];
+    const pattern = /\[([^,]+)-(\d)\]/;
+    input.split(',').forEach((item) => {
+      const name = item.match(pattern)[1];
+      const quantity = item.match(pattern)[2];
+      data.push({'name': name, 'quantity': quantity});
     });
   }
 }
